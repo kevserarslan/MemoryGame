53e4fc79280fd6465b53d2d000a78ee9cbb62d7f
kevser Arslan <kevser_arslan@stu.fsm.edu.tr>
2025-03-17 23:15:06 +0300

    MemoryGame

---
+++ HafizaOyunu/src/hafizaoyunu/Kart.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hafizaoyunu;
+
+/**
+ *
+ * @author Kevser
+ */
+public class Kart {
+    private char deger;
+    private boolean tahmin =false;
+
+    public Kart(char deger) {
+        this.deger = deger;
+    }
+
+    public char getDeger() {
+        return deger;
+    }
+
+    public void setDeger(char deger) {
+        this.deger = deger;
+    }
+
+    public boolean isTahmin() {
+        return tahmin;
+    }
+
+    public void setTahmin(boolean tahmin) {
+        this.tahmin = tahmin;
+    }
+    
+    
+    
+}
